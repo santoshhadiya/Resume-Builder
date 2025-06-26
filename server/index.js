73e688bc7fb7eb76f998fbd9fc4853f556f309da
santosh hadiya <santoshhadiya033@gmail.com>
2025-06-26 23:24:00 +0530

    :new

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ const authRoutes = require('./routes/auth');
 const resumeRoutes = require('./routes/resumes');
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(bodyParser.json({ limit: '10mb' }));
